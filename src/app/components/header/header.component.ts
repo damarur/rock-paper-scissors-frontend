b1215e9a2995ee0299b33dd915aad2114997d51e
David Martinez Urrea <david.martinez.dev@gmail.com>
2025-03-19 20:58:53 +0100

    CSS adjustements

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,17 +1,22 @@
 import { Component } from '@angular/core';
-import { MatToolbar } from '@angular/material/toolbar';
+import { MatToolbar, MatToolbarModule } from '@angular/material/toolbar';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
-import { MatOption, MatSelect } from '@angular/material/select';
+import {
+  MatOption,
+  MatSelect,
+  MatSelectModule,
+} from '@angular/material/select';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
-  imports: [TranslateModule, MatToolbar, MatIconModule, MatSelect, MatOption],
+  imports: [TranslateModule, MatToolbarModule, MatIconModule, MatSelectModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
   selectedLanguage: string = '';
+
   constructor(private readonly translate: TranslateService) {
     this.selectedLanguage = translate.currentLang ?? 'en';
   }
--- src/app/interceptors/api-key.interceptor.ts
+++ src/app/interceptors/api-key.interceptor.ts
@@ -1,10 +1,13 @@
-import {HttpEvent, HttpHandlerFn, HttpRequest} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {environment} from '../../environments/environment';
+import { HttpEvent, HttpHandlerFn, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
-export function apiKeyInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
+export function apiKeyInterceptor(
+  req: HttpRequest<unknown>,
+  next: HttpHandlerFn
+): Observable<HttpEvent<unknown>> {
   const newReq = req.clone({
-    headers: req.headers.append('X-Api-Key', environment.apiKey),
+    headers: req.headers.append('x-api-key', environment.apiKey),
   });
   return next(newReq);
 }
